9f90966e96fea6ee1c2d8ecb4b28b83f2c8cf256
unknown <mateusvpdl@outlook.com>
2024-05-10 22:14:00 -0300

    corrigindo erro de img do Next

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,16 +1,17 @@
+import Image from 'next/image';
 import styles from './Footer.module.scss';
 
 export default function Footer(){
     return(
         <footer className={styles.container}>
             <a href="https://www.linkedin.com/in/mateus9349/">
-                <img className={styles.icon} src="assets\icons\linkedin.png"/>
+                <Image alt='icon' className={styles.icon} src="assets\icons\linkedin.png"/>
             </a>
             <a href="https://github.com/Mateus9349">
-                <img className={styles.icon} src="assets/icons/github.png"/>
+                <Image alt='icon' className={styles.icon} src="assets/icons/github.png"/>
             </a>
             <a href="https://www.instagram.com/mateus_passos1/">
-                <img className={styles.icon} src="assets/icons/instagram.png" />
+                <Image alt='icon' className={styles.icon} src="assets/icons/instagram.png" />
             </a>
         </footer>
     )
